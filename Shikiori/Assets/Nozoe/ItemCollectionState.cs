1563585288359afcc3cfc80ca2c7a812e240c6bb
Nozoeyuki <fko2647069@stu.o-hara.ac.jp>
2026-09-23 22:21:26 +0900

    更新

--- Shikiori/Assets/Nozoe/ItemCollectionState.cs
+++ Shikiori/Assets/Nozoe/ItemCollectionState.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
 
-public class ItemManager : MonoBehaviour
+
+public class ItemCollectionState: ScriptableObject
 {
     // 「合計何個拾ったか」を覚えておく
     private int _collectedCount;
 
-    // 一個拾った時に呼んでもらう。数字を増やすだけ。
-    public void Collect()
+    private void OnEnable()
     {
-        _collectedCount++;
+        _collectedCount = 0;
+    }
+
+    // 一個拾った時に呼んでもらう。数字を増やすだけ。
+    public void Collect(ItemData itemData)
+    {
+        _collectedCount += itemData.ScoreValue;
     }
 
     // 今の合計数を教えてあげる
     public int CollectedCount => _collectedCount;
+
+
 }
